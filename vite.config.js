87b4578632b230d23cd1036695f4a2af86e6078a
michaelbroadbent <michael.m.broadbent@gmail.com>
2026-03-04 12:51:00 -0500

    github pages config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/basketball-hotboard/',
   plugins: [react()],
   server: { port: 3000, open: true },
   build: { outDir: 'dist', sourcemap: true }
